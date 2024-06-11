390a98b53ed43650a95070caada40b0cf6268699
JorgeGuzman <jorguzman100@gmail.com>
2024-06-11 20:08:52 +0300

    10 Hostinger - File structure updated to upload seemlesly

--- server.mjs
+++ server.mjs
@@ -27,7 +27,7 @@ app.use(cors({
 app.use(express.json());
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public_html')));
 
 // Serve static files from the 'node_modules' directory
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
